5f236e44fdc267fbe571bdd9be4d94ce6b993528
Abd Alrahman Fani <faniabdo99@gmail.com>
2024-04-23 21:02:44 +0200

    Fixed the projects dropzone issue

--- routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\ProjectController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('upload/{project}', [ProjectController::class, 'uploadGallery'])->name('admin.projects.uploadGallery');
